de42d69b7512f0574cdce243c3a0db39db102c8e
qqq <kos.y.6094@gmail.com>
2024-06-30 15:06:32 +0300

    useState

--- react-app/src/components/Buttton/Button.jsx
+++ react-app/src/components/Buttton/Button.jsx
@@ -1,12 +1,17 @@
 import './Button.css';
+import { useState } from 'react';
 
 function Button() {
+  const [text, setText] = useState('Сохранить');
+  console.log('Ререндер');
+
   const clicked = () => {
-    console.log('тест');
+    setText(t => t + '!');
+    console.log(text);
   };
 
   return (
-      <button onClick={clicked} className='button accent'>Сохранить</button>      
+      <button onClick={clicked} className='button accent'>{text}</button>      
   );
 }
 
