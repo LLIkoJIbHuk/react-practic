d7dfa16b0acc4eeeac56f9c77aad9e639433989a
qqq <kos.y.6094@gmail.com>
2024-07-01 17:55:39 +0300

    combining components

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,7 +1,12 @@
 import './App.css';
 import Button from './components/Buttton/Button';
 import CardButton from './components/CardButton/CardButton';
+import Header from './components/Header/Header';
+import JournalAddButton from './components/JournalAddButton/JournalAddButton';
 import JournalItem from './components/JournalItem/JournalItem';
+import JournalList from './components/JournalList/JournalList';
+import Body from './layouts/Body/Body';
+import LeftPanel from './layouts/LeftPanel/LeftPanel';
 
 function App() {
 
@@ -19,25 +24,32 @@ function App() {
   ];
 
   return (
-    <>
-      <h1>Заголовок</h1>
-      <p>Текст</p>
+    <div className='app'>
+      <LeftPanel>
+        <Header/>
+        <JournalList>
+          <JournalAddButton/>
+          <CardButton>
+            <JournalItem
+              title={data[0].title}
+              text={data[0].text}
+              date={data[0].date}
+            />
+          </CardButton>
+          <CardButton>
+            <JournalItem
+            title={data[1].title}
+            text={data[1].text}
+            date={data[1].date}
+            />
+          </CardButton>
+        </JournalList>
+      </LeftPanel>
+      <Body>
+        Body
+      </Body>
       <Button/>
-      <CardButton>
-        <JournalItem
-          title={data[0].title}
-          text={data[0].text}
-          date={data[0].date}
-        />
-      </CardButton>
-      <CardButton>
-        <JournalItem
-        title={data[1].title}
-        text={data[1].text}
-        date={data[1].date}
-        />
-      </CardButton>
-    </> 
+    </div> 
   );
 }
 
--- react-app/src/components/CardButton/CardButton.jsx
+++ react-app/src/components/CardButton/CardButton.jsx
@@ -1,12 +1,14 @@
 import './CardButton.css';
 
-function CardButton({children}) {
+function CardButton({children, className}) {
+
+  const cl = 'card-button' + (className ? ' ' + className : '');
 
   return (
     <>
-      <button className='card-button'>
+      <button className={cl}>
         {children}
-      </button>      
+      </button>
     </> 
   );
 }
