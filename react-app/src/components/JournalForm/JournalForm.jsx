491c93ae552c5bbe2ab611ba0e08769c9ce3c365
qqq <kos.y.6094@gmail.com>
2024-07-15 09:28:46 +0300

    fixed clearing form

--- react-app/src/components/JournalForm/JournalForm.jsx
+++ react-app/src/components/JournalForm/JournalForm.jsx
@@ -29,27 +29,24 @@ function JournalForm({onSubmit}) {
   useEffect(() => {
     if (isFormReadyToSubmit) {
       onSubmit(values);
-      // dispatchForm({type: 'CLEAR'});
+      dispatchForm({type: 'CLEAR'});
     }
   }, [isFormReadyToSubmit]);
 
-  // const onChange = (e) => {
-  //   dispatchForm({type: 'SET_VALUE', payload: {[e.target.name]: e.target.value}});
-  // };
+  const onChange = (e) => {
+    dispatchForm({type: 'SET_VALUE', payload: {[e.target.name]: e.target.value}});
+  };
 
   {/*Проверка валидности формы (заполнение)*/}
   const addJournalItem = (e) => {
     e.preventDefault();
-    const formData = new FormData(e.target);
-    const formProps = Object.fromEntries(formData);
-    dispatchForm({type: 'SUBMIT', payload: formProps});
-    // dispatchForm({type: 'SUBMIT'});
+    dispatchForm({type: 'SUBMIT'});
   };
 
   return (
     <form className={styles['journal-form']} onSubmit={addJournalItem}>
       <div>
-        <input type='text' name='title' className={cn(styles['input-title'], {
+        <input type='text' onChange={onChange} value={values.title} name='title' className={cn(styles['input-title'], {
           [styles['invalid']] : !isValid.title
         })} />
       </div>
@@ -58,7 +55,7 @@ function JournalForm({onSubmit}) {
           <img src='/calendar.svg' alt='Иконка календаря'/>
           <span>Дата</span>
         </label>
-        <input type='date' name='date'  id="date" className={cn(styles['input'], {
+        <input type='date' name='date' onChange={onChange} value={values.date} id="date" className={cn(styles['input'], {
           [styles['invalid']] : !isValid.date
         })}/>
       </div>
@@ -67,10 +64,10 @@ function JournalForm({onSubmit}) {
           <img src='/folder.svg' alt='Иконка папки'/>
           <span>Метки</span>
         </label>
-        <input type='text'  id="tag"  name='tag' className={styles['input']} />
+        <input type='text'  id="tag" onChange={onChange} value={values.tag} name='tag' className={styles['input']} />
       </div>
       
-      <textarea name='post' id=''  cols='30' rows='10' className={cn(styles['input'], {
+      <textarea name='post' id='' onChange={onChange} value={values.post} cols='30' rows='10' className={cn(styles['input'], {
           [styles['invalid']] : !isValid.post
         })}></textarea>
       <Button text='Сохранить'/>
--- react-app/src/components/JournalForm/JournalForm.state.js
+++ react-app/src/components/JournalForm/JournalForm.state.js
@@ -5,9 +5,10 @@ export const INITIAL_STATE = {
     date: true
   },
   values: {
-    post: undefined,
-    title: undefined,
-    date: undefined
+    post: '',
+    title: '',
+    date: '',
+    tag: ''
   },
   isFormReadyToSubmit: false
 };
@@ -24,13 +25,11 @@ export function formReducer(state, action) {
     case 'RESET_VALIDITY':
       return {...state, isValid: INITIAL_STATE.isValid};
     case 'SUBMIT': {
-      const titleValidity = action.payload.title?.trim().length;
-      const postValidity = action.payload.post?.trim().length;
-      const dateValidity = action.payload.date;
+      const titleValidity = state.values.title?.trim().length;
+      const postValidity = state.values.post?.trim().length;
+      const dateValidity = state.values.date;
       return {
         ...state,
-        /* Храним состояние в state */
-        values: action.payload,
         /* Ставим правильную валидность (замена if-else) */
         isValid: {
           post: postValidity,
