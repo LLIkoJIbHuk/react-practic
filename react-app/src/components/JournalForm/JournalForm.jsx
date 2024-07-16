ee42a5c6ad82920de505f3b9f52d61403dcc03b3
qqq <kos.y.6094@gmail.com>
2024-07-16 13:05:00 +0300

    forwardRef

--- react-app/src/components/JournalForm/JournalForm.jsx
+++ react-app/src/components/JournalForm/JournalForm.jsx
@@ -3,6 +3,7 @@ import styles from './JournalForm.module.css';
 import Button from '../Buttton/Button';
 import cn from 'classnames';
 import { formReducer, INITIAL_STATE } from './JournalForm.state';
+import Input from '../Input/Input';
 
 function JournalForm({onSubmit}) {
 
@@ -65,25 +66,21 @@ function JournalForm({onSubmit}) {
   return (
     <form className={styles['journal-form']} onSubmit={addJournalItem}>
       <div>
-        <input type='text' ref={titleRef} onChange={onChange} value={values.title} name='title' className={cn(styles['input-title'], {
-          [styles['invalid']] : !isValid.title
-        })} />
+        <Input type='text' ref={titleRef} isValid={isValid.title} onChange={onChange} value={values.title} name='title' appearence="title" />
       </div>
       <div className={styles['form-row']}>
         <label htmlFor="date" className={styles['form-label']}>
           <img src='/calendar.svg' alt='Иконка календаря'/>
           <span>Дата</span>
         </label>
-        <input type='date' ref={dateRef} name='date' onChange={onChange} value={values.date} id="date" className={cn(styles['input'], {
-          [styles['invalid']] : !isValid.date
-        })}/>
+        <Input type='date' ref={dateRef} name='date' isValid={isValid.date} onChange={onChange} value={values.date} id="date"/>
       </div>
       <div className={styles['form-row']}>
         <label htmlFor="tag" className={styles['form-label']}>
           <img src='/folder.svg' alt='Иконка папки'/>
           <span>Метки</span>
         </label>
-        <input type='text'  id="tag" onChange={onChange} value={values.tag} name='tag' className={styles['input']} />
+        <Input type='text'  id="tag" onChange={onChange} value={values.tag} name='tag'/>
       </div>
       
       <textarea name='post' ref={postRef} id='' onChange={onChange} value={values.post} cols='30' rows='10' className={cn(styles['input'], {
