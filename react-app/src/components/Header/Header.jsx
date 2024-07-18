a808777d778ec12a5bf47b7f20260e8b4f9cc899
qqq <kos.y.6094@gmail.com>
2024-07-18 15:16:35 +0300

    change user

--- react-app/src/components/Header/Header.jsx
+++ react-app/src/components/Header/Header.jsx
@@ -1,9 +1,18 @@
+import SelectUser from '../SelectUser/SelectUser';
 import styles from './Header.module.css';
 
-function Header() {
+function Header(changedUser) {
+
+  const changeUser = (e) => {
+    changedUser(e.target.value);
+    console.log(e.target.value);
+  };
 
   return (
-    <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+    <>
+      <img className={styles.logo} src="/logo.svg" alt="Логотип журнала" />
+      <SelectUser changeUser={changeUser} />
+    </>
   );
 }
 
