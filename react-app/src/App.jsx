96a86c1318f5652d0b6198d07180a62fa0399245
qqq <kos.y.6094@gmail.com>
2024-07-21 11:02:59 +0300

    Revert "creating hook"
    This reverts commit 9ef9e27384965f95698829eca1a32a0fc5da65ff.

--- react-app/src/App.jsx
+++ react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import Header from './components/Header/Header';
 import JournalAddButton from './components/JournalAddButton/JournalAddButton';
@@ -5,45 +6,53 @@ import JournalForm from './components/JournalForm/JournalForm';
 import JournalList from './components/JournalList/JournalList';
 import Body from './layouts/Body/Body';
 import LeftPanel from './layouts/LeftPanel/LeftPanel';
-import { useLocalStorage } from './hooks/use-localstorage.hook';
-import { UserContextProvider } from './context/user.context';
-
-function mapItems(items){
-  if(!items){
-    return [];
-  } 
-  return items.map(i => ({
-      ...i,
-      date: new Date(i.date)
-    }));
-}
 
 function App() {
   {/* Создали состояние */}
-  const [items, setItems] = useLocalStorage('data');
+  const [items, setItems] = useState([]);
+
+  {/*Заставляем выполнить 1 раз во время появления компонента*/}
+  useEffect(() => {
+    {/*Если есть данные - парсим; устанавливаем item*/}
+    const data = JSON.parse(localStorage.getItem('data'));
+    if (data) {
+      setItems(data.map(item => ({
+        ...item,
+        date: new Date(item.date)
+      })));
+  }
+  }, []);
+
+  {/*Устанавливаем item по ключу data, передаем строку*/}
+  useEffect(() => {
+    if(items.length){
+      console.log('Запись');
+      localStorage.setItem('data', JSON.stringify(items));
+    }
+  }, [items]);
+  {/*Проверка делается только внутри хука*/}
 
   {/* Функция для установки нового состояния */}
   const addItem = item => {
-    setItems([...mapItems(items), {
-      ...item,
+    setItems(oldItems => [...oldItems, {
+      post: item.post,
+      title: item.title,
       date: new Date(item.date),
-      id: items.length > 0 ? Math.max(...items.map(i => i.id)) + 1 : 1
+      id: oldItems.length > 0 ? Math.max(...oldItems.map(i => i.id)) + 1 : 1
     }]);
   };
 
   return (
-    <UserContextProvider>
-      <div className='app'>
-        <LeftPanel>
-          <Header/>
-          <JournalAddButton/>
-          <JournalList items={mapItems(items)} />
-        </LeftPanel>
-        <Body>
-          <JournalForm onSubmit={addItem}/>
-        </Body>
-      </div> 
-    </UserContextProvider>
+    <div className='app'>
+      <LeftPanel>
+        <Header/>
+        <JournalAddButton/>
+        <JournalList items={items} />
+      </LeftPanel>
+      <Body>
+        <JournalForm onSubmit={addItem}/>
+      </Body>
+    </div> 
   );
 }
 
