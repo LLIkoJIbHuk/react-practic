38139079f13047f152a7706a970e0196df6dad66
qqq <kos.y.6094@gmail.com>
2024-08-02 20:34:40 +0300

    creating routes

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,6 +1,10 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
+import { Route, Routes } from 'react-router-dom';
+import { Menu } from './pages/Menu/Menu';
+import { Cart } from './pages/Cart/Cart';
+import { Error } from './pages/Error/Error';
 
 function App() {
 
@@ -15,6 +19,15 @@ function App() {
       <Button onClick={addCounter} >Кнопка</Button>
       <Button  appearence='big' onClick={addCounter} >Кнопка</Button>
       <Input placeholder='Email' />
+      <div>
+        <a href='/' >Меню</a>
+        <a href='/cart' >Корзина</a>
+      </div>
+      <Routes>
+        <Route path='/' element={<Menu />} />
+        <Route path='/cart' element={<Cart />} />
+        <Route path='*' element={<Error />} />
+      </Routes>
     </>
   );
 }
