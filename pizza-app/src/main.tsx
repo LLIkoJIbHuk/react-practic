f7f95a8252c2eae424f39e7de48febd13683c0c9
qqq <kos.y.6094@gmail.com>
2024-08-03 16:59:00 +0300

    outlet

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -5,15 +5,22 @@ import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import { Menu } from './pages/Menu/Menu';
 import { Cart } from './pages/Cart/Cart';
 import { Error } from './pages/Error/Error';
+import { Layout } from './layout/Menu/Menu';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Menu/>
-  },
-  {
-    path: '/cart',
-    element: <Cart/>
+    element: <Layout/>,
+    children: [
+      {
+        path: '/',
+        element: <Menu/>
+      },
+      {
+        path: '/cart',
+        element: <Cart/>
+      }
+    ]
   },
   {
     path: '*',
