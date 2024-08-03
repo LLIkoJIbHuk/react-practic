70f5ceb0d96bfa39a4fd350407029f42f6ef34ae
qqq <kos.y.6094@gmail.com>
2024-08-03 14:50:37 +0300

    link

--- pizza-app/src/App.tsx
+++ pizza-app/src/App.tsx
@@ -1,25 +1,6 @@
 import { MouseEvent, useState } from 'react';
 import Button from './components/Button/Button';
 import Input from './components/Input/Input';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-import { Menu } from './pages/Menu/Menu';
-import { Cart } from './pages/Cart/Cart';
-import { Error } from './pages/Error/Error';
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <Menu/>
-  },
-  {
-    path: '/cart',
-    element: <Cart/>
-  },
-  {
-    path: '*',
-    element: <Error/>
-  }
-]);
 
 function App() {
 
@@ -34,11 +15,6 @@ function App() {
       <Button onClick={addCounter} >Кнопка</Button>
       <Button  appearence='big' onClick={addCounter} >Кнопка</Button>
       <Input placeholder='Email' />
-      <div>
-        <a href='/' >Меню</a>
-        <a href='/cart' >Корзина</a>
-      </div>
-      <RouterProvider router={router} />
     </>
   );
 }
