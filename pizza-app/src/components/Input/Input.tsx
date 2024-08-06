89ed6c4f93e9ecd4a9eae1cc2073e9859418fda4
qqq <kos.y.6094@gmail.com>
2024-08-06 12:39:48 +0300

    input and search

--- pizza-app/src/components/Input/Input.tsx
+++ pizza-app/src/components/Input/Input.tsx
@@ -1,7 +1,7 @@
 import { forwardRef } from 'react';
 import styles from './Input.module.css';
 import cn from 'classnames';
-import { InputProps } from './Input.pops';
+import { InputProps } from './Input.props';
 
 const Input = forwardRef<HTMLInputElement, InputProps> (function Input({className, isValid = true, ...props}, ref) {
 
---
+++ pizza-app/src/components/Search/Search.tsx
@@ -0,0 +1,18 @@
+import { forwardRef } from 'react';
+import styles from './Search.module.css';
+import cn from 'classnames';
+import { SearchProps } from './Search.props';
+
+const Search = forwardRef<HTMLInputElement, SearchProps> (function Input({className, isValid = true, ...props}, ref) {
+
+  return (
+    <div className={styles['input-wrapper']} >
+      <input ref={ref} className={cn(styles['input'], className, styles['input'], {
+          [styles['invalid']] : isValid
+        })} {...props} />
+        <img className={styles['icon']} src='/search-icon.svg' alt='Иконка поиска' />
+    </div>
+  );
+});
+
+export default Search;
