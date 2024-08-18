62bdd0cc2ee359e20a656ca207c538a5c4f88a72
qqq <kos.y.6094@gmail.com>
2024-08-18 12:09:53 +0300

    creating store

--- pizza-app/src/main.tsx
+++ pizza-app/src/main.tsx
@@ -12,6 +12,8 @@ import { AuthLayout } from './layout/Auth/AuthLayout';
 import { Login } from './pages/Login/Login';
 import { Register } from './pages/Register/Register';
 import { RequireAutht } from './helpers/RequireAuth';
+import { Provider } from 'react-redux';
+import { store } from './store/store.ts';
 
 const Menu = lazy(() => import('./pages/Menu/Menu'));
 
@@ -80,6 +82,8 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <Provider store={store} >
+      <RouterProvider router={router} />
+    </Provider>
   </React.StrictMode>
 );
