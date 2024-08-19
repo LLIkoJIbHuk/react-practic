3e9140036ed713c955bb3dff48d630904e2f3981
qqq <kos.y.6094@gmail.com>
2024-08-19 19:14:16 +0300

    auth via Redux Toolkit

--- pizza-app/src/helpers/RequireAuth.tsx
+++ pizza-app/src/helpers/RequireAuth.tsx
@@ -1,8 +1,11 @@
+import { RootState } from '../store/store';
 import { ReactNode } from 'react';
+import { useSelector } from 'react-redux';
 import { Navigate } from 'react-router-dom';
 
 export const RequireAutht = ({children}: {children: ReactNode}) => {
-  const jwt = localStorage.getItem('jwt');
+  //берет состояние и вытаскивает нужный jwt токен
+  const jwt = useSelector((s: RootState) => s.user.jwt);
   
   if(!jwt) {
     return <Navigate to='/auth/login' replace />;
