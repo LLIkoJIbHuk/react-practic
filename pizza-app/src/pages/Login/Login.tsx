b37de6cb1be9e2ba2dccd6ba68635a683014c53b
qqq <kos.y.6094@gmail.com>
2024-08-20 16:14:32 +0300

    request in actions

--- pizza-app/src/pages/Login/Login.tsx
+++ pizza-app/src/pages/Login/Login.tsx
@@ -3,13 +3,10 @@ import Button from '../../components/Button/Button';
 import Headling from '../../components/Headling/Headling';
 import Input from '../../components/Input/Input';
 import styles from './Login.module.css';
-import { FormEvent, useState } from 'react';
-import axios, { AxiosError } from 'axios';
-import { PREFIX } from '../../helpers/API';
-import { LoginResonse } from '../../interfaces/auth.interface';
-import { useDispatch } from 'react-redux';
-import { AppDispatch } from '../../store/store';
-import { userActions } from '../../store/user.slice';
+import { FormEvent, useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { AppDispatch, RootState } from '../../store/store';
+import { login } from '../../store/user.slice';
 
 export type LoginForm = {
   email: {
@@ -25,6 +22,14 @@ export function Login(){
   const [error, setError] = useState<string | null>();
   const navigate = useNavigate();
   const dispatch = useDispatch<AppDispatch>();
+  const jwt = useSelector((s: RootState) => s.user.jwt);
+
+  //отслеживаем jwt и при его появлении логиним пользователя
+  useEffect(() => {
+    if(jwt){
+      navigate('/');
+    }
+  }, [jwt, navigate]);
 
   const submit = async (e: FormEvent) => {
     e.preventDefault();
@@ -35,21 +40,24 @@ export function Login(){
   };
 
   const sendLogin = async (email: string, password: string) => {
-    try{
-      const {data} = await axios.post<LoginResonse>(`${PREFIX}/auth/login`, {
-        email,
-        password
-      });
-      console.log(data);
-      dispatch(userActions.addJwt(data.access_token));
-      navigate('/');
-    } catch (e) {
-      //проверяем, что ошибка вызвана axios
-      if (e instanceof AxiosError){
-        console.log(e);
-        setError(e.response?.data.message);
-      }
-    }
+
+    dispatch(login({email, password}));
+
+    // try{
+    //   const {data} = await axios.post<LoginResonse>(`${PREFIX}/auth/login`, {
+    //     email,
+    //     password
+    //   });
+    //   console.log(data);
+    //   dispatch(userActions.addJwt(data.access_token));
+    //   navigate('/');
+    // } catch (e) {
+    //   //проверяем, что ошибка вызвана axios
+    //   if (e instanceof AxiosError){
+    //     console.log(e);
+    //     setError(e.response?.data.message);
+    //   }
+    // }
   };
 
   return <div className={styles['login']}>
--- pizza-app/src/store/user.slice.ts
+++ pizza-app/src/store/user.slice.ts
@@ -1,5 +1,8 @@
-import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { loadState } from './storage';
+import { LoginResonse } from '../interfaces/auth.interface';
+import axios from 'axios';
+import { PREFIX } from '../helpers/API';
 
 export const JWT_PERSISTENT_STATE = 'userData';
 
@@ -8,24 +11,41 @@ export interface UserPersistentState {
 }
 
 export interface UserState {
-  jwt: string | null
+  jwt: string | null;
+  loginState: null | 'rejected';
 }
 
 //при загрузке загружаем из localStorage, если нет - ставим null
 const initialState: UserState = {
-  jwt: loadState<UserPersistentState>(JWT_PERSISTENT_STATE)?.jwt ?? null
+  jwt: loadState<UserPersistentState>(JWT_PERSISTENT_STATE)?.jwt ?? null,
+  loginState: null
 };
+
+export const login = createAsyncThunk('user/login', 
+  async (params: {email: string, password: string}) => {
+    const {data} = await axios.post<LoginResonse>(`${PREFIX}/auth/login`, {
+      email: params.email,
+      password: params.password
+    });
+    return data;
+  }
+);
 
 export const userSlice = createSlice({
   name: 'user',
   initialState,
-  reducers: {
-    addJwt: (state, action: PayloadAction<string>) => {
-      state.jwt = action.payload;
-    },
+  reducers: { 
     logout: (state) => {
       state.jwt = null;
     }
+  },
+  extraReducers: (builder) => {
+    builder.addCase(login.fulfilled, (state, action: PayloadAction<LoginResonse>) => {
+      state.jwt = action.payload.access_token;
+    });
+    builder.addCase(login.rejected, (state, action) => {
+      action.error.message
+    });
   }
 });
 
