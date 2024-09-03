c033bf4d41ecb076e6a7b4276e7c703f96e20c91
qqq <kos.y.6094@gmail.com>
2024-09-03 20:27:07 +0300

    Вёрстка Categories

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,5 +1,11 @@
+import { Categories, Container, SortPopup, Title, TopBar } from "@/components/shared";
+
 export default function Home() {
   return <>
-    
+    <Container className="mt-10" >
+      <Title text="Все пиццы" size="lg" className="font-extrabold" />
+    </Container>
+    <TopBar />
+    <div style={{height: '3000px'}} ></div>
   </>;
 }
