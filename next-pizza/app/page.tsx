54a4b46428fb1c7a46823259b279a7cb2a1f5da7
qqq <kos.y.6094@gmail.com>
2024-09-04 18:27:26 +0300

    Вёрстка Checkbox и фильтрации

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -1,4 +1,4 @@
-import { Categories, Container, SortPopup, Title, TopBar } from "@/components/shared";
+import { Categories, Container, Filters, SortPopup, Title, TopBar } from "@/components/shared";
 
 export default function Home() {
   return <>
@@ -6,6 +6,19 @@ export default function Home() {
       <Title text="Все пиццы" size="lg" className="font-extrabold" />
     </Container>
     <TopBar />
-    <div style={{height: '3000px'}} ></div>
+    <Container className="mt-10 pb-14" >
+      <div className="flex gap-[60px]" >
+
+        {/* Фильтрация */}
+        <div className="w-[250px]" >
+          <Filters />
+        </div>
+
+        {/* Список товаров */}
+        <div className="flex-1" >
+          <div className="flex flex-col gap-16" >Список товаров</div>
+        </div>
+      </div>
+    </Container>
   </>;
 }
