353dd5d7938cb6ac6d7882c24ee1c574fdc3e434
qqq <kos.y.6094@gmail.com>
2024-09-05 18:15:56 +0300

    RangeSlider

---
+++ next-pizza/components/shared/checkbox-filters-group.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+interface Props {
+  title: string;
+  items: Item[];
+  defaultItems?: Item[];
+  limit?: number;
+  searchInputPlaceholder?: string;
+  className?: string;
+}
+
+export const CheckboxFiltersGroup: React.FC<Props> = ({ className }) => {
+  return <></>;
+};
