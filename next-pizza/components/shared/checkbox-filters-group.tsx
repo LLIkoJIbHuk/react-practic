b70866b3b3c3536f912db81e3a4c678053f4b707
qqq <kos.y.6094@gmail.com>
2024-09-09 19:26:34 +0300

    Доработан поиск в фильтрации

--- next-pizza/components/shared/checkbox-filters-group.tsx
+++ next-pizza/components/shared/checkbox-filters-group.tsx
@@ -30,16 +30,26 @@ export const CheckboxFiltersGroup: React.FC<Props> = (
   }
 ) => {
   const [showAll, setShowAll] = React.useState(false);
+  const [searchValue, setSearchValue] = React.useState('');
+
+  const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearchValue(e.target.value);
+  }
 
   {/* Рендерить все элементы - true. Иначе - отрендерить до лимита */}
-  const list = showAll ? items : defaultItems.slice(0, limit);
+  const list = showAll 
+    ? items.filter((item) => item.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()))
+    : defaultItems.slice(0, limit);
 
   return <div className={className} >
     <p className='font-bold mb-3' >{title}</p>
 
     {showAll && (
       <div className='mb-5' >
-        <Input placeholder={searchInputPlaceholder} className='bg-gray-50 border-none' />
+        <Input 
+          onChange={onChangeSearchInput}
+          placeholder={searchInputPlaceholder}
+          className='bg-gray-50 border-none' />
       </div>
     )}
 
