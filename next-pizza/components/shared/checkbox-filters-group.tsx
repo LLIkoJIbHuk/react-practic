6a3094ac950a480d9c970466a180a80c478c009f
qqq <kos.y.6094@gmail.com>
2024-09-10 19:18:31 +0300

    Верстка списка продуктов

--- next-pizza/components/shared/checkbox-filters-group.tsx
+++ next-pizza/components/shared/checkbox-filters-group.tsx
@@ -34,46 +34,48 @@ export const CheckboxFiltersGroup: React.FC<Props> = (
 
   const onChangeSearchInput = (e: React.ChangeEvent<HTMLInputElement>) => {
     setSearchValue(e.target.value);
-  }
+  };
 
   {/* Рендерить все элементы - true. Иначе - отрендерить до лимита */}
   const list = showAll 
-    ? items.filter((item) => item.text.toLocaleLowerCase().includes(searchValue.toLocaleLowerCase()))
+    ? items.filter((item) => item.text.toLowerCase().includes(searchValue.toLocaleLowerCase()))
     : defaultItems.slice(0, limit);
 
-  return <div className={className} >
-    <p className='font-bold mb-3' >{title}</p>
+  return (
+    <div className={className} >
+      <p className='font-bold mb-3' >{title}</p>
 
-    {showAll && (
-      <div className='mb-5' >
-        <Input 
-          onChange={onChangeSearchInput}
-          placeholder={searchInputPlaceholder}
-          className='bg-gray-50 border-none' />
+      {showAll && (
+        <div className='mb-5' >
+          <Input 
+            onChange={onChangeSearchInput}
+            placeholder={searchInputPlaceholder}
+            className='bg-gray-50 border-none' />
+        </div>
+      )}
+
+      <div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar' >
+        {list.map((item, index) => (
+          <FilterCheckbox 
+            onCheckedChange={(ids) => console.log(ids)}
+            checked={false}
+            key={String(item.value)}
+            value={item.value}
+            text={item.text}
+            endAdornment={item.endAdornment}
+          />
+        ))}
       </div>
-    )}
 
-    <div className='flex flex-col gap-4 max-h-96 pr-2 overflow-auto scrollbar' >
-      {list.map((item, index) => (
-        <FilterCheckbox 
-          onCheckedChange={(ids) => console.log(ids)}
-          checked={false}
-          key={String(item.value)}
-          value={item.value}
-          text={item.text}
-          endAdornment={item.endAdornment}
-        />
-      ))}
+      {/* Если количество элементов больше лимита - выводим кнопку */}
+      {items.length > limit && (
+        <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''}>
+          <button onClick={() => setShowAll(!showAll)} className="text-primary mt-3">
+            {showAll ? 'Скрыть' : '+ Показать все'}
+          </button>
+        </div>
+      )}
+
     </div>
-
-    {/* Если количество элементов больше лимита - выводим кнопку */}
-    {items.length > limit && (
-      <div className={showAll ? 'border-t border-t-neutral-100 mt-4' : ''} >
-        <button onClick={() => setShowAll(!showAll)} className="text-primary mt-3">
-          {showAll ? 'Скрыть' : '+ Показать все'}
-        </button>
-      </div>
-    )}
-
-  </div>
+  );
 };
