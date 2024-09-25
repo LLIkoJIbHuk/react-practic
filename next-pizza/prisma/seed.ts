ac76beeff1d0e9ec46bf9540f1ab9aaebc23bf1e
qqq <kos.y.6094@gmail.com>
2024-09-25 19:35:26 +0300

    seed up/down, bcrypt

--- next-pizza/prisma/seed.ts
+++ next-pizza/prisma/seed.ts
@@ -1,9 +1,29 @@
-async function up(){
+import { prisma } from "./prisma-client";
+import { hashSync } from "bcrypt";
 
+async function up(){
+  await prisma.user.createMany({
+    data: [
+      {
+        fullName: 'User',
+        email: 'user@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'USER',
+      },
+      {
+        fullName: 'Admin',
+        email: 'admin@test.ru',
+        password: hashSync('111111', 10),
+        verified: new Date(),
+        role: 'ADMIN',
+      }
+    ]
+  });
 }
 
 async function down(){
-
+  await prisma.user.deleteMany({});
 }
 
 async function main(){
