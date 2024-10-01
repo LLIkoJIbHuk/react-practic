4ddf7ef0afe024e4dd3f2f6c3d9ba9750587d8f8
qqq <kos.y.6094@gmail.com>
2024-10-01 15:09:22 +0300

    анимация search-input

--- next-pizza/components/shared/search-input.tsx
+++ next-pizza/components/shared/search-input.tsx
@@ -29,6 +29,14 @@ export const SearchInput: React.FC<Props> = ({ className }) => {
           placeholder="Найти пиццу..."
           onFocus={() => setFocused(true)}
         />
+
+        <div className={cn(
+          'absolute w-full bg-white rounded-xl py-2 top-14 shadow-md transition-all duration-200 invisible opacity-0 z-30',
+          focused && 'visible opacity-100 top-12'
+        )}>
+        qweqwe
+        </div>
+
       </div>
     </>
   );
