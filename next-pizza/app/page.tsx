ca481967722836812a7926cb8b7149d572e45210
qqq <kos.y.6094@gmail.com>
2024-10-22 17:13:15 +0300

    отображение только существующих категорий в топбаре

--- next-pizza/app/page.tsx
+++ next-pizza/app/page.tsx
@@ -20,7 +20,7 @@ export default async function Home() {
     <Container className="mt-10" >
       <Title text="Все пиццы" size="lg" className="font-extrabold" />
     </Container>
-    <TopBar />
+    <TopBar categories={categories.filter((category) => category.products.length > 0)} />
     <Container className="mt-10 pb-14" >
       <div className="flex gap-[80px]" >
 
