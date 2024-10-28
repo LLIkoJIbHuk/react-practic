04a965e588f2e66a1b4435368d31bc4b1ecd03cf
qqq <kos.y.6094@gmail.com>
2024-10-28 19:01:10 +0300

    функционал закрытия модального окна

--- next-pizza/components/shared/modals/choose-product-modal.tsx
+++ next-pizza/components/shared/modals/choose-product-modal.tsx
@@ -5,6 +5,7 @@ import { cn } from "@/lib/utils";
 import { Product } from "@prisma/client";
 import React from "react";
 import { Title } from "../title";
+import { useRouter } from "next/navigation";
 
 interface Props {
   className?: string;
@@ -12,8 +13,11 @@ interface Props {
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
+  const router = useRouter();
+
   return (
-    <Dialog open={Boolean(product)} >
+    /* модальное окно */
+    <Dialog open={Boolean(product)} onOpenChange={() => router.back()} >
       <DialogContent className={cn('p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden', className)} >
         <Title>{product.name}</Title>
       </DialogContent>
