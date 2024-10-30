8136f49035399184d4884ee3c4459bf9a20799e1
qqq <kos.y.6094@gmail.com>
2024-10-30 18:19:16 +0300

    проверка на пиццу для рендеринга модального окна

--- next-pizza/components/shared/modals/choose-product-modal.tsx
+++ next-pizza/components/shared/modals/choose-product-modal.tsx
@@ -2,24 +2,30 @@
 
 import { DialogContent, Dialog } from "@/components/ui/dialog";
 import { cn } from "@/lib/utils";
-import { Product } from "@prisma/client";
 import React from "react";
 import { useRouter } from "next/navigation";
 import { ChooseProductForm } from "../choose-product-form";
+import { ProductWithRelations } from "@/@types/prisma";
 
 interface Props {
   className?: string;
-  product: Product;
+  product: ProductWithRelations;
 }
 
 export const ChooseProductModal: React.FC<Props> = ({ product, className }) => {
   const router = useRouter();
+  /* Проверяем, является ли продукт пиццей */
+  const isPizzaForm = Boolean(product.items[0].pizzaType);
 
   return (
     /* модальное окно */
     <Dialog open={Boolean(product)} onOpenChange={() => router.back()} >
       <DialogContent className={cn('p-0 w-[1060px] max-w-[1060px] min-h-[500px] bg-white overflow-hidden', className)} >
-        <ChooseProductForm imageUrl={product.imageUrl} name={product.name} ingredients={[]} />
+        { /* проверяем, является ли продукт пиццей, иначе рендерим стандартную форму */
+          isPizzaForm ? (
+            'PizzaForm'
+          ) : <ChooseProductForm imageUrl={product.imageUrl} name={product.name} ingredients={[]} />
+        }
       </DialogContent>
     </Dialog>
   );
