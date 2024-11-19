763602b9ae978561a125b9a4106315e3759aacfe
qqq <kos.y.6094@gmail.com>
2024-11-19 21:06:52 +0300

    обработка запросов корзины

--- next-pizza/app/api/cart/[id]/route.ts
+++ next-pizza/app/api/cart/[id]/route.ts
@@ -1,10 +1,41 @@
-import { NextRequest } from "next/server";
+import { prisma } from "@/prisma/prisma-client";
+import { updateCartTotalAmount } from "@/shared/lib/update-cart-total-amount";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(req: NextRequest, { params }: { params: { id: string } }) {
   try {
-    const { id } = params;
-    const body = await req.json();
+    const id = Number(params.id);
+    const data = (await req.json()) as { quantity: number };
+    const token = req.cookies.get('cartToken')?.value;
+
+    if (!token) {
+      return NextResponse.json({message: 'Токен корзины не найден'});
+    }
+
+    const cartItem = await prisma.cartItem.findFirst({
+      where: {
+        id,
+      },
+    });
+
+    if (!cartItem) {
+      return NextResponse.json({message: 'Товар не найден'});
+    }
+
+    await prisma.cartItem.update({
+      where: {
+        id,
+      },
+      data: {
+        quantity: data.quantity,
+      },
+    });
+
+    const updatedUserCart = await updateCartTotalAmount(token);
+
+    return NextResponse.json(updatedUserCart);
   } catch (error) {
-    console.log(error);
+    console.log('[CART PATCH] Server error', error);
+    return NextResponse.json({message: 'Не удалось обновить корзину'}, {status: 500});
   }
 }
