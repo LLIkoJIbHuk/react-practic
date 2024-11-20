538ce48f9937c89c17963de24c0979e57383e4df
qqq <kos.y.6094@gmail.com>
2024-11-20 19:54:07 +0300

    запрос на обновление корзины

--- next-pizza/shared/store/cart.ts
+++ next-pizza/shared/store/cart.ts
@@ -26,7 +26,7 @@ export const useCartStore = create<CartState>((set, get) => ({
   fetchCartItems: async () => {
     try {
       set({ loading: true, error: false });
-      const data = await Api.cart.fetchCart();
+      const data = await Api.cart.getCart();
       set(getCartDetails(data));
     } catch (error) {
       console.log(error);
@@ -36,7 +36,8 @@ export const useCartStore = create<CartState>((set, get) => ({
     }
   },
   
-  removeCartItem: async (id: number) => {},
   updateItemQuantity: async (id: number, quantity: number) => {},
+
+  removeCartItem: async (id: number) => {},
   addCartItem: async (values: any) => {},
 }));
