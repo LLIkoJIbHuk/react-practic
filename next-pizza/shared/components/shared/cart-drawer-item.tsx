7ee6e7d88fb853dc7b1405c33d190ce66a265f21
qqq <kos.y.6094@gmail.com>
2024-11-21 19:28:52 +0300

    обновление количества товара в корзине

--- next-pizza/shared/components/shared/cart-drawer-item.tsx
+++ next-pizza/shared/components/shared/cart-drawer-item.tsx
@@ -7,6 +7,7 @@ import { CountButton } from "./count-button";
 import { Trash2Icon } from "lucide-react";
 
 interface Props extends CartItemProps {
+  onClickUpdateQuantity?: (type: 'plus' | 'minus') => void;
   className?: string;
 }
 
--- next-pizza/shared/services/cart.ts
+++ next-pizza/shared/services/cart.ts
@@ -2,13 +2,9 @@ import { axiosInstance } from "./instance";
 import { CartDTO } from "./dto/cart.dto";
 
 export const getCart = async (): Promise<CartDTO> => {
-  const { data } = await axiosInstance.get<CartDTO>('/cart');
-
-  return data;
+  return (await axiosInstance.get<CartDTO>('/cart')).data;
 };
 
-export const updateItemQuantity = async (id: number, quantity: number): Promise<CartDTO> => {
-  const { data } = await axiosInstance.patch<CartDTO>('/cart/' + id, { quantity });
-
-  return data
+export const updateItemQuantity = async (itemId: number, quantity: number): Promise<CartDTO> => {
+  return (await axiosInstance.patch<CartDTO>('/cart/' + itemId, { quantity })).data;
 };
