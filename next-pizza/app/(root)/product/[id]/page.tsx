cc17da0d2c1cd6098c01c496a1a3a7ca62170224
qqq <kos.y.6094@gmail.com>
2024-12-07 16:53:59 +0300

    Suspense

--- next-pizza/app/(root)/product/[id]/page.tsx
+++ next-pizza/app/(root)/product/[id]/page.tsx
@@ -1,4 +1,4 @@
-import { Container, ProductImage, Title } from "@/shared/components/shared";
+import { Container, PizzaImage, Title } from "@/shared/components/shared";
 import { GroupVariants } from "@/shared/components/shared/group-variants";
 import { prisma } from "@/prisma/prisma-client";
 import { notFound } from "next/navigation";
@@ -17,7 +17,7 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
   return (
     <Container className="flex flex-col my-10" >
       <div className="flex flex-1" >
-        <ProductImage imageUrl={product.imageUrl} size={40}/>
+        <PizzaImage imageUrl={product.imageUrl} size={40}/>
 
         <div className="w-[490px] bg-[#f7f6f5] p-7" >
           <Title text={product.name} size="md" className="font-extrabold mb-1" />
@@ -25,7 +25,7 @@ export default async function ProductPage({ params: { id } }: { params: { id: st
           <p className="text-gray-400" >Lorem ipsum dolor sit amet.</p>
 
           <GroupVariants 
-            selectedValue="2"
+            value="2"
             items={[
               {
               name: 'Маленькая',
