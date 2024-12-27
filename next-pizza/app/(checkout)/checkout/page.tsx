fd0dcb42f4ba59501147b51ecf93ff4b540fb078
qqq <kos.y.6094@gmail.com>
2024-12-27 20:14:29 +0300

    валидация через FormProvider

--- next-pizza/app/(checkout)/checkout/page.tsx
+++ next-pizza/app/(checkout)/checkout/page.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { useForm } from "react-hook-form";
+import { FormProvider, useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import { CheckoutSidebar, Container, Title } from "@/shared/components/shared";
 import { useCart } from "@/shared/hooks";
@@ -22,6 +22,10 @@ export default function CheckoutPage() {
     }
   });
 
+  const onSubmit = (data: CheckoutFormValues) => {
+    console.log(data);
+  };
+
   const onClickCountButton = (id: number, quantity: number, type: 'plus' | 'minus') => {
     const newQuantity = type === 'plus' ? quantity + 1 : quantity - 1;
     updateItemQuantity(id, newQuantity);
@@ -30,20 +34,24 @@ export default function CheckoutPage() {
   return <Container className="mt-10" >
     <Title text="Оформление заказа" className="font-extrabold mb-8 text-[36px]" />
 
-    <div className="flex gap-10" >
-      {/* Левая колонка */}
-      <div className="flex flex-col gap-10 flex-1 mb-20" >
-        <CheckoutCart onClickCountButton={onClickCountButton} removeCartItem={removeCartItem} items={items} />
+    <FormProvider {...form} >
+      <form onSubmit={form.handleSubmit(onSubmit)} >
+        <div className="flex gap-10" >
+          {/* Левая колонка */}
+          <div className="flex flex-col gap-10 flex-1 mb-20" >
+            <CheckoutCart onClickCountButton={onClickCountButton} removeCartItem={removeCartItem} items={items} />
 
-        <CheckoutPersonalForm />
+            <CheckoutPersonalForm />
 
-        <CheckoutAddressForm />
-      </div>
+            <CheckoutAddressForm />
+          </div>
 
-      {/* Правая часть */}
-      <div className="w-[450px]">
-        <CheckoutSidebar totalAmount={totalAmount} />
-      </div>
-    </div>
+          {/* Правая часть */}
+          <div className="w-[450px]">
+            <CheckoutSidebar totalAmount={totalAmount} />
+          </div>
+        </div>
+      </form>
+    </FormProvider>
   </Container>
 }
