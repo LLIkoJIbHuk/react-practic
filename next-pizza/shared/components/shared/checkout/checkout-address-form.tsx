0f6188ca4a9ec9bd52ef4cdf4f44586f4408bd87
qqq <kos.y.6094@gmail.com>
2024-12-28 16:01:26 +0300

    валидация адреса

--- next-pizza/shared/components/shared/checkout/checkout-address-form.tsx
+++ next-pizza/shared/components/shared/checkout/checkout-address-form.tsx
@@ -2,16 +2,28 @@ import React from 'react';
 import { WhiteBlock } from '../white-block';
 import { FormTextarea } from '../form';
 import { AddressInput } from '../address-input';
+import { Controller, useFormContext } from 'react-hook-form';
+import { ErrorText } from '../error-text';
 
 interface Props {
   className?: string;
 }
 
 export const CheckoutAddressForm: React.FC<Props> = ({ className }) => {
+
+  const {control} = useFormContext();
+
   return (
     <WhiteBlock title="3. Адрес доставки" >
       <div className="flex flex-col gap-5" >
-        <AddressInput />
+        <Controller
+          control={control}
+          name="address"
+          render={({ field, fieldState }) => <>
+            <AddressInput onChange={field.onChange} />
+            {fieldState.error?.message && <ErrorText text={fieldState.error.message} />}
+          </>}
+        />
 
         <FormTextarea
           name="comment"
