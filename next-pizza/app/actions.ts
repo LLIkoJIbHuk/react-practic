e328f05dcbb73a5083dbfa20dda908797c3dc69e
qqq <kos.y.6094@gmail.com>
2024-12-31 15:02:56 +0300

    создание заказа

--- next-pizza/app/actions.ts
+++ next-pizza/app/actions.ts
@@ -3,25 +3,79 @@
 import { prisma } from "@/prisma/prisma-client";
 import { CheckoutFormValues } from "@/shared/constants";
 import { OrderStatus } from "@prisma/client";
+import { cookies } from "next/headers";
 
 export async function createOrder(data: CheckoutFormValues) {
-  console.log(data);
+  try {
+    const cookieStore = cookies();
+    const cartToken = cookieStore.get('cartToken')?.value;
 
-  const token = '123';
-
-  await prisma.order.create({ 
-    data: {
-      token,
-      totalAmount: 1500,
-      status: OrderStatus.PENDING,
-      items: [],
-      fullName: data.firstName + ' ' + data.lastName,
-      email: data.email,
-      phone: data.phone,
-      address: data.address,
-      comment: data.comment
+    if (!cartToken) {
+      throw new Error('Токен корзины не найден');
     }
-  });
 
-  return 'google.com';
+    //Находим корзину по токену
+    const userCart = await prisma.cart.findFirst({
+      include: {
+        user: true,
+        items: {
+          include: {
+            ingredients: true,
+            productItem: {
+              include: {
+                product: true
+              },
+            },
+          },
+        },
+      },
+      where: {
+        token: cartToken,
+      },
+    });
+
+    //Если корзина не найдена возвращаем ошибку
+    if (!userCart) {
+      throw new Error('Корзина не найдена');
+    }
+
+    if (userCart?.totalAmount === 0) {
+      throw new Error('Корзина пуста');
+    }
+
+    /* Создаем заказ */
+    const order = await prisma.order.create({
+      data: {
+        token: cartToken,
+        fullName: data.firstName + ' ' + data.lastName,
+        email: data.email,
+        phone: data.phone,
+        address: data.address,
+        comment: data.comment,
+        totalAmount: userCart.totalAmount,
+        status: OrderStatus.PENDING,
+        items: JSON.stringify(userCart.items),
+      },
+    });
+
+    // Очищаем корзину после оформления заказа
+    await prisma.cart.update({
+      where: {
+        id: userCart.id,
+      },
+      data: {
+        totalAmount: 0,
+      },
+    });
+
+    // Удаление товаров
+    await prisma.cartItem.deleteMany({
+      where: {
+        cartId: userCart.id,
+      },
+    });
+
+  } catch (err) {
+    console.log(err);
+  }
 }
