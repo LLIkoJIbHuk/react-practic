fa41870cf1773604d69c6fc48c2530c336901835
qqq <kos.y.6094@gmail.com>
2025-01-14 09:00:53 +0300

    подтверждение аккаунта с помощью верификационного кода

--- next-pizza/app/api/verify/route.ts
+++ next-pizza/app/api/verify/route.ts
@@ -1,9 +1,45 @@
-import { NextRequest } from "next/server";
+import { prisma } from "@/prisma/prisma-client";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(req: NextRequest) {
-  
   try {
+    //извлекаем код из URL
+    const code = req.nextUrl.searchParams.get('code');
 
+    if (!code) {
+      return NextResponse.json({ error: 'Код не найден' }, { status: 400 });
+    }
+
+    const verificationCode = await prisma.verificationCode.findFirst({
+      where: {
+        code,
+      },
+    });
+
+    //проверяем на наличие в БД
+    if (!verificationCode) {
+      return NextResponse.json({ error: 'Неверный код' }, { status: 400 });
+    }
+
+    //присваиваем дату верификации, если код верный
+    await prisma.user.update({
+      where: {
+        id: verificationCode.userId,
+      },
+      data: {
+        verified: new Date(),
+      },
+    });
+
+    //удаляем код из БД
+    await prisma.verificationCode.delete({
+      where: {
+        id: verificationCode.id,
+      },
+    });
+
+    //возвращаем пользователя на главную страницу
+    return NextResponse.redirect(new URL('/?verified', req.url));
   } catch (error) {
     console.log(error);
     console.log('[VERIFY_GET] Server error', error);
